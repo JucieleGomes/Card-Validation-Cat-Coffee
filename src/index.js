900fbd05e7bdde637e33fe421d9a8e38fef503df
JucieleGomes <jucielegomes.s@hotmail.com>
2023-07-28 20:38:28 -0300

    Projeto alterado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+function preenchimentoCompleto() {
+  if (document.getElementById('cardNumberInput').value.length < 16) {
+    alert('Por favor, insira todos os 16 dígitos do cartão');
+  } else {
+    const cardNumber = document.getElementById('cardNumberInput').value;
+    const ehValido = validator.isValid(cardNumber);
+    
+    if (ehValido) {
+      alert('Cartão válido!');
+    } else {
+      alert('Cartão inválido!');
+    }
+  }
+}
+
+const button = document.querySelector('.submitButton');
+button.addEventListener('click', preenchimentoCompleto);
+
+const cardNumberInput = document.getElementById('cardNumberInput');
+cardNumberInput.addEventListener('input', function() {
+  const cartaoMascarado = validator.maskify(cardNumberInput.value);
+  cardNumberInput.value = cartaoMascarado;
+});
+
+
+
+
+
+
+
+
+
+
+
